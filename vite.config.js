c0a502fc95c345df2f7d4e151ad95767c08ef8c3
anmol11p <pandayanmol6@gmail.com>
2024-10-09 13:44:12 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/dashbord/', // Set to your repository name for GitHub Pages
 });
